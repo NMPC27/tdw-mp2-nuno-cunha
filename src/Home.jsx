9b4042df949ac8598e9b3cc99533a802e9cce278
nunocunha <nunocunha@ua.pt>
2023-11-29 23:09:24 +0000

    added home and about pages

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,21 @@ import AppBarHeader from "./components/AppBarHeader";
 import { useDispatch } from "react-redux";
 import { setNavbarValue } from "./features/navbarSlice";
 import { useEffect } from "react";
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: "left",
+  color: theme.palette.text.secondary,
+  borderRadius: "10px",
+}));
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -13,7 +28,36 @@ export default function Home() {
   return (
     <>
       <AppBarHeader />
-      <h1>Home</h1>
+      <Box>
+        <Container maxWidth="sm" sx={{marginBottom:"3vw"}} >
+          <h1 style={{textAlign:"center"}}>FIFA Players</h1>
+          <Typography variant="h5" align="center" color="text.secondary" paragraph>
+          Welcome to the epicenter of virtual soccer brilliance! Ready to elevate your FIFA experience? Let's play! ⚽🎮
+          </Typography>
+        </Container>
+      </Box>
+      <Grid container spacing={5}>
+        <Grid item xs={1.5}></Grid>
+        <Grid item xs={3}>
+          <Item style={{height:"17vw"}}>
+            <h3 style={{marginTop:"0.5vw", marginBottom:"0.5vw",textAlign:"center"}}>Players Info</h3>
+            <p>Peel back the layers of your favorite players with our concise player info section. Uncover the essentials - from the name that echoes on the pitch to the birthdate that marks their soccer journey. Height, weight, gender - every detail at your fingertips. It's more than data; it's the human touch behind the game. Ready to explore? ⚽🔍</p>
+          </Item>
+        </Grid>
+        <Grid item xs={3}>
+          <Item style={{height:"17vw"}}>
+            <h3 style={{marginTop:"0.5vw", marginBottom:"0.5vw",textAlign:"center"}}>Players Stats</h3>
+            <p>Dive into the heart of player performance with our comprehensive stats breakdown. From the overall rating that defines excellence to the nuances of pace, shooting, passing, dribbling, defending, and physicality, every facet of their game is laid bare. Explore their preferred position, dominant foot, and skill moves that set them apart. With details on their weak foot, get ready to analyze the complete picture of your favorite player's prowess. Stats aficionados, your playground awaits! ⚽📈</p>
+          </Item>
+        </Grid>
+        <Grid item xs={3}>
+          <Item style={{height:"17vw"}}>
+            <h3 style={{marginTop:"0.5vw", marginBottom:"0.5vw",textAlign:"center"}} >Players Trifecta</h3>
+            <p>Unlock the allegiance of your favorite players with our concise breakdown. Discover the club where they weave magic, the league where battles are fought, and the nation they proudly represent. It's the ultimate trifecta encapsulating a player's journey from local arenas to international glory. Dive into the heart of football loyalty – club, league, nation – their soccer story in three powerful words. Ready to explore the player's world? ⚽🏟️🌍</p>
+          </Item>
+        </Grid>
+        <Grid item xs={1.5}></Grid>
+      </Grid>
     </>
   );
 }
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,6 +3,18 @@ import Box from "@mui/material/Box";
 import { useDispatch } from "react-redux";
 import { setNavbarValue } from "../features/navbarSlice";
 import { useEffect } from "react";
+import Grid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: "left",
+  color: theme.palette.text.secondary,
+  borderRadius: "10px",
+}));
 
 export default function AboutPage() {
   const dispatch = useDispatch();
@@ -15,8 +27,36 @@ export default function AboutPage() {
     <>
       <AppBarHeader />
       <Box sx={{ ml: "2vw", mr: "2vw" }}>
-        <h1>AboutPage</h1>
+        <Grid container spacing={2}>
+          <Grid item xs={1}></Grid>
+          <Grid item xs={11}>
+            <h2
+              style={{
+                color: "#273746",
+                marginTop: "0vw",
+                marginBottom: "0vw",
+              }}
+            >
+              About
+            </h2>
+          </Grid>
+          <Grid item xs={1}></Grid>
+          <Grid item xs={4}>
+            <img src="me.jpg" width="100%" style={{ borderRadius:"10px" }} />
+          </Grid>
+          <Grid item xs={6}>
+            <Item>
+              <h1 >Nuno Cunha</h1>
+              <p>Hi, I'm Nuno Cunha! I'm a 5th year Informatics Engineering student at the University of Aveiro. I'm passionate about web development and internet of things. I'm also an avid soccer fan, and I'm always looking for ways to combine my interests. I hope you enjoy this project!</p>
+              <p>Within the realm of my Technologies and Web Development course, I'm engrossed in an ambitious project. Fueled by React, this dynamic dashboard taps into external APIs, delivering real-time FIFA player data. Covering player essentials, detailed metrics, and affiliations with clubs, leagues, and nations, the platform promises a rich, immersive experience. The addition of a user-friendly search function enhances accessibility, catering to both casual users and dedicated FIFA enthusiasts. Join me on this journey as I delve into the intricacies of web development, API integration, and the fascinating world of player statistics!</p>
+              <p>Crafting the FIFA Players Dashboard for my Technologies and Web Development course has been a condensed crash course in React, API integration, and continuous integration. This hands-on experience has sharpened my UI design skills, solidified my understanding of real-time data updates, and streamlined my development workflow.</p>
+              <p>Ready to explore? ⚽🎮</p>
+            </Item>
+          </Grid>
+        </Grid>
+        
       </Box>
+
     </>
   );
 }
